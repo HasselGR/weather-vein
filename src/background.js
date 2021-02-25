a8c5b52f16cbb35da0a0adbb180654762055674f
David Gamboa <gamboadavid1998@gmail.com>
2021-02-24 23:00:50 -0400

    UI tweaks 1

--- src/background.js
+++ src/background.js
@@ -67,7 +67,7 @@ const updater = browser.alarms.create('Hourly Updater', {
   periodInMinutes: 60,
 })
 
-//hourly updater, it fetchs the data every hour when it listens to the alarm
+// hourly updater, it fetchs the data every hour when it listens to the alarm
 browser.alarms.onAlarm.addListener(() => {
   const date = new Date()
   browser.storage.local.get()
@@ -77,18 +77,11 @@ browser.alarms.onAlarm.addListener(() => {
         .then(response => response.json())
         .then(data => {
           console.log(data)
-          const temperature = parseFloat((data.main.temp - 273).toFixed(2))
-          const minimumTemp = parseFloat((data.main.temp_min - 273).toFixed(2))
-          const maximumTemp = parseFloat((data.main.temp_max - 273).toFixed(2))
-          const feelsTemp = parseFloat((data.main.feels_like - 273).toFixed(2))
+          const temperature = (data.main.temp - 273)
           browser.storage.local.set({
             weather: data.weather[0].main,
-            icon: data.weather[0].icon,
             description: data.weather[0].description,
             temperature: `${temperature}ºC`,
-            min_temperature: `${minimumTemp}ºC`,
-            max_temperature: `${maximumTemp}ºC`,
-            feel_temperature: `${feelsTemp}ºC`,
             humidity: data.main.humidity,
             date: date.toString(),
           })
@@ -99,7 +92,7 @@ browser.alarms.onAlarm.addListener(() => {
 // when you install the extension.
 
 browser.runtime.onInstalled.addListener(function () {
-  fetch('http://api.ipstack.com/check?access_key=46d55efd82cf8067ed7c334ec534bb52')//first we use an API to get the IP location
+  fetch('http://api.ipstack.com/check?access_key=46d55efd82cf8067ed7c334ec534bb52')// first we use an API to get the IP location
     .then(response => response.json())
     .then(data => {
       city = data.city // then we get the city and the country code so we are able to fetch the climate for our city.
@@ -107,30 +100,19 @@ browser.runtime.onInstalled.addListener(function () {
       // eslint-disable-next-line promise/no-nesting
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=15d7968a4ea50d6b568a07a54e399358`)// No entiendo porque si dejo de anidar los fetchs no me funciona el scope.
         .then(response => response.json())
-        .then(info => {//once we get the info we declare it and then we store it on the extension storage
-          // console.log('city', city)
-          // console.log('country', country)
-          // console.log('weather from ipstack', info)
-
-          //This is for converting the temperature as it comes in kelvin... we modify it first and then we store it.
-          const temperature = parseFloat((info.main.temp - 273).toFixed(2)) //NEEDED, AND NEEDS TO BE AN INT
-          const minimumTemp = parseFloat((info.main.temp_min - 273).toFixed(2))//not needed
-          const maximumTemp = parseFloat((info.main.temp_max - 273).toFixed(2)) //not needed
-          const feelsTemp = parseFloat((info.main.feels_like - 273).toFixed(2)) //not needed
-          const date = new Date()
-          // console.log('date', date)
+        .then(info => { // once we get the info we declare it and then we store it on the extension storage
+          // This is for converting the temperature as it comes in kelvin... we modify it first and then we store it.
+          const temperature = Math.floor(info.main.temp - 273) // NEEDED, AND NEEDS TO BE AN INT
+          let date = new Date()
+        
           browser.storage.local.set({ // saving the data
             country: country, // needed to locate the weather
             city: city, // NEEDED, goes in the popup
-            weather: info.weather[0].main, //NEEDED
-            icon: info.weather[0].icon,//we will use our own icons so we dont need this
-            description: info.weather[0].description, //not needed
-            temperature: `${temperature}ºC`, // NEEDED
-            min_temperature: `${minimumTemp}ºC`, //not needed
-            max_temperature: `${maximumTemp}ºC`,//not needed
-            feel_temperature: `${feelsTemp}ºC`,//not needed
-            humidity: info.main.humidity,//NEEDED
-            date: date.toString(),//NEEDED
+            weather: info.weather[0].main, // NEEDED
+            description: info.weather[0].description, // needed
+            temperature: `${temperature}º`, // NEEDED
+            humidity: `${info.main.humidity}%`, // NEEDED
+            date: date.toDateString(), // NEEDED
           })
         })
     }).catch(error => console.log(error))
--- src/popup.js
+++ src/popup.js
@@ -2,26 +2,27 @@
 import browser from 'webextension-polyfill'
 
 
-//first we get all the elemets to fill the info up, with the new layout some of them will be not required
+// first we get all the elemets to fill the info up, with the new layout some of them will be not required
 
 
-let location = document.getElementById('city')
-let place = document.getElementById('place')
-let temperature = document.getElementById('temperature')
+let city = document.getElementById('city')
+let date = document.getElementById('date')
 let weather = document.getElementById('weather')
+let temperature = document.getElementById('temperature')
+let imageweather = document.getElementById('imageweather')
 let humidity = document.getElementById('humidity')
 let footer = document.getElementById('footer')
-let input = document.getElementById('userinput')
-let button = document.getElementById('submit')
 
-//not needed as we wont add something to change cities
+
+// not needed as we wont add something to change cities
+
 const clearNode = (node) => {
   while (node.firstChild) {
     node.removeChild(node.firstChild)
   }
 }
 
-//pretty self explanatory, it just adds the headline, we do it this way for i18 purposes, you send the size of the header, what its going to say and where it will be attached to
+// pretty self explanatory, it just adds the headline, we do it this way for i18 purposes, you send the size of the header, what its going to say and where it will be attached to
 
 
 const addTitle = (type, string, size = 'h1') => {
@@ -29,7 +30,7 @@ const addTitle = (type, string, size = 'h1') => {
   header.appendChild(document.createTextNode(`${string}`))
   type.appendChild(header)
 }
-//for not so important info. same as before
+// for not so important info. same as before
 
 const addInfo = (type, string, info) => {
   let header = document.createElement('p')
@@ -37,7 +38,11 @@ const addInfo = (type, string, info) => {
   type.appendChild(header)
 }
 
-//to add images, we should change this.
+const changeText = (location, string) => {
+  location.innerHTML = `${string}`
+}
+
+// to add images, we should change this.
 const addImage = (type, string) => {
   let header = document.createElement('img')
   header.setAttribute('width', '150')
@@ -45,74 +50,188 @@ const addImage = (type, string) => {
   header.setAttribute('src', `http://openweathermap.org/img/wn/${string}@2x.png`)
   type.appendChild(header)
 }
-//this is the bread and butter, we take the data from the local storage and we add it to the popup.
+
+const getWeather = (clima) => {
+  switch (clima) {
+    case 'Thunderstorm':
+      return 'Tormenta'
+    case 'Drizzle':
+      return 'Llovizna'
+    case 'Rain':
+      return 'Lluvia'
+    case 'Snow':
+      return 'Granizo'
+    case 'Mist':
+    case 'Smoke':
+    case 'Haze':
+    case 'Dust':
+    case 'Fog':
+    case 'Sand':
+    case 'Ash':
+    case 'Squall':
+    case 'Tornado':
+      return 'Niebla'
+    case 'Clear':
+      return 'Despejado'
+    case 'Clouds':
+      return 'Nublado'
+
+  }
+
+}
+
+const getWeatherImg = (description) => {
+  switch (description) {
+    case 'thunderstorm with light rain':
+    case 'thunderstorm with rain':
+    case 'thunderstorm with heavy rain':
+    case 'light thunderstorm':
+    case 'thunderstorm':
+    case 'heavy thunderstorm':
+    case 'ragged thunderstorm':
+    case 'thunderstorm with light drizzle':
+    case 'thunderstorm with heavy drizzle':
+      return 'thunderstorm'
+    case 'light intensity drizzle':
+    case 'drizzle':
+    case 'heavy intensity drizzle':
+    case 'light intensity drizzle rain':
+    case 'drizzle rain':
+    case 'heavy intensity drizzle rain':
+    case 'shower rain and drizzle':
+    case 'heavy shower rain and drizzle':
+    case 'shower drizzle':
+    case 'light intensity shower rain':
+    case 'shower rain':
+    case 'heavy intensity shower rain':
+    case 'ragged shower rain':
+      return 'drizzle'
+    case 'light rain':
+    case 'moderate rain':
+    case 'heavy intensity rain':
+    case 'very heavy rain':
+    case 'extreme rain':
+      return 'rain'
+    case 'freezing rain':
+    case 'light snow':
+    case 'snow':
+    case 'heavy snow':
+    case 'sleet':
+    case 'Light shower sleet':
+    case 'Shower sleet':
+    case 'Light rain and snow':
+    case 'rain and snow':
+    case 'light shower snow':
+    case 'shower snow':
+    case 'heavy shower snow':
+      return 'snow'
+    case 'mist':
+    case 'smoke':
+    case 'haze':
+    case 'sand whirls':
+    case 'dust whirls':
+    case 'fog':
+    case 'sand':
+    case 'dust':
+    case 'volcanic ash':
+    case 'squalls':
+    case 'tornado':
+      return 'mist'
+    case 'clear sky':
+      return 'clear'
+    case 'few clouds':
+      return 'few clouds'
+    case 'scattered clouds':
+      return 'scattered clouds'
+    case 'broken clouds':
+      return 'broken clouds'
+    case 'overcast clouds':
+      return 'overcast clouds'
+  }
+}
+
+const translateDate = (date) => {
+  let day = ''
+  let month = ''
+  let number = date.slice(8, 10)
+  switch (date.slice(0, 3)) {
+    case 'Mon':
+      day = 'Lunes'
+      break
+    case 'Tue':
+      day = 'Martes'
+      break
+    case 'Wed':
+      day = 'Miercoles'
+      break
+    case 'Thu':
+      day = 'Jueves'
+      break
+    case 'Fri':
+      day = 'Viernes'
+      break
+    case 'Sat':
+      day = 'Sábado'
+      break
+    case 'Sun':
+      day = 'Domingo'
+      break
+  }
+  switch (date.slice(4, 7)) {
+    case 'Jan':
+      month = 'Enero'
+      break
+    case 'Feb':
+      month = 'Febrero'
+      break
+    case 'Mar':
+      month = 'Marzo'
+      break
+    case 'Apr':
+      month = 'Abril'
+      break
+    case 'May':
+      month = 'Mayo'
+      break
+    case 'Jun':
+      month = 'Junio'
+      break
+    case 'Jul':
+      month = 'Julio'
+      break
+    case 'Aug':
+      month = 'Agosto'
+      break
+    case 'Sep':
+      month = 'Septiembre'
+      break
+    case 'Oct':
+      month = 'Octubre'
+      break
+    case 'Nov':
+      month = 'Noviembre'
+      break
+    case 'Dec':
+      month = 'Diciembre'
+      break
+  }
+  return `${day} ${number} de ${month}`
+}
+// this is the bread and butter, we take the data from the local storage and we add it to the popup.
 const addWeather = () => {
-  const city = browser.storage.local.get()
-  city.then(data => {
+  const storage = browser.storage.local.get()
+  storage.then(data => {
     console.log(data)
-    addTitle(place, `Ciudad: ${data.city}`, 'h3')
-    addTitle(temperature, browser.i18n.getMessage('temperature'))
-    addTitle(weather, browser.i18n.getMessage('weather'))
-    addTitle(humidity, browser.i18n.getMessage('humidity'))
-    addInfo(weather, browser.i18n.getMessage('weatherParameter'), data.weather)
-    addImage(weather, data.icon)
-    addInfo(weather, browser.i18n.getMessage('weatherDescription'), data.description)
-    addInfo(temperature, browser.i18n.getMessage('temperatureWeather'), data.temperature)
-    addInfo(temperature, browser.i18n.getMessage('minimumTemperature'), data.min_temperature)
-    addInfo(temperature, browser.i18n.getMessage('maximumTemperature'), data.max_temperature)
-    addInfo(temperature, browser.i18n.getMessage('feelTemperature'), data.feel_temperature)
-    addInfo(humidity, browser.i18n.getMessage('humidityWeather'), `${data.humidity}%`)
-    addInfo(footer, 'Last updated since', `${data.date.substr(0, 10)} ${data.date.substr(11, 14)}`)
+    
+    // const description = getWeatherImg(data.description)
+    // const fecha = translateDate(data.date)
+    changeText(city, data.city)
+    changeText(date, translateDate(data.date))
+    changeText(weather, getWeather(data.weather))
+    changeText(imageweather, `<img src="images/assets/${getWeatherImg(data.description)}.png" width="300" />`)
+    changeText(temperature, data.temperature)
+    changeText(humidity, data.humidity)
   }).catch(error => console.log(error))
 }
-//method for changing the city, i think this is not needed anymore either
-const changeCity = () => {
-  const req = browser.storage.local.get('country')
-  req.then(data => {
-    let assignment = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value},${data.country}&appid=15d7968a4ea50d6b568a07a54e399358`)
-    assignment.then(response => {
-      if (response.status === 200) {
-        console.log('valid combination')
-        response.json()
-          .then(info => {
-            const temperatureNew = parseFloat((info.main.temp - 273).toFixed(2))
-            const minimumTemp = parseFloat((info.main.temp_min - 273).toFixed(2))
-            const maximumTemp = parseFloat((info.main.temp_max - 273).toFixed(2))
-            const feelsTemp = parseFloat((info.main.feels_like - 273).toFixed(2))
-            const date = new Date()
-            console.log('date', date)
-            browser.storage.local.set({
-              city: input.value,
-              weather: info.weather[0].main,
-              icon: info.weather[0].icon,
-              description: info.weather[0].description,
-              temperature: `${temperatureNew}ºC`,
-              min_temperature: `${minimumTemp}ºC`,
-              max_temperature: `${maximumTemp}ºC`,
-              feel_temperature: `${feelsTemp}ºC`,
-              humidity: info.main.humidity,
-              date: date.toString(),
-            })
-          })
-          .catch(error => console.log(error))
-      } else {
-        console.log('Not a valid combination')
-      }
-    })
-  }).catch(error => console.log(error))
-}
-
-//just in case we change the storage, not needed
-browser.storage.onChanged.addListener(() => {
-  clearNode(place)
-  clearNode(temperature)
-  clearNode(weather)
-  clearNode(humidity)
-  clearNode(footer)
-  addWeather()
-})
-
-// button.addEventListener('click', changeCity)
-
 
 addWeather()
